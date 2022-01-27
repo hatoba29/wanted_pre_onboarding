08b60a21f9fa0497adedc08c023a8d86e4bbd1bc
hatoba29 <hatoba29@naver.com>
2022-01-27 15:39:01 +0900

    Update Toggle

--- src/component/Toggle.js
+++ src/component/Toggle.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 const Toggle = () => {
-  return <div id="toggle"></div>;
+  const [isOn, setOn] = useState(false);
+
+  const update = (e) => {
+    const toggleBg = e.currentTarget;
+    const toggleBtn = e.currentTarget.firstElementChild;
+
+    setOn(!isOn);
+    toggleBg.classList.toggle("on");
+    toggleBtn.classList.toggle("on");
+  };
+
+  return (
+    <div id="toggle" className="view">
+      <div className="toggle-bg" onClick={update}>
+        <div className="toggle-btn" />
+      </div>
+      <div className="display">Toggle Switch {isOn ? "ON" : "OFF"}</div>
+    </div>
+  );
 };
 
 export default Toggle;
